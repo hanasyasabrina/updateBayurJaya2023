0f2211548d4cfc506e79686305eca36a3e557313
Nur Oktavionita <nur.salassa@student.umn.ac.id>
2023-03-24 23:50:04 +0700

    updateContact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,26 +2,27 @@
     <div class="contact w-full relative px-4 xl:px-48">
         <!-- mulai dari sini -->
 
-      <div class="grid grid-cols-3 gap">
+      <div class="container mx-auto">
+        <div class="grid grid-cols-1 gap md:grid-cols-2 lg:grid-cols-3">
 
-        <div class="contact--title text-xl font-semibold w-48 h-7 left-44 top-60 text-center">KANTOR PUSAT</div>
-        <div class="contact--title text-xl font-semibold w-48 h-7 left-44 top-60 text-center">WORKSHOP</div>
-        <div class="contact--title text-xl font-semibold w-48 h-7 left-44 top-60 text-center">DIVISI TELKOM</div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-4"><img class="img--asset float-left mb-14 mr-4" :src="require('../assets/ornament/pin.png')">Jl. Raya Bayur No. 72 Periuk Jaya Tangerang Banten 15131</div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-4"><img class="img--asset float-left mb-14 mr-4" :src="require('../assets/ornament/pin.png')">Kp. Sekarwangi No. 39-40 Neglasari Tangerang Banten 15129</div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-4"><img class="img--asset float-left mb-14 mr-4" :src="require('../assets/ornament/pin.png')">Jl. Mandanau Blok A5 No 29 Tangerang Banten 15133</div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-3" v-for="(index, key) in dataEmail" :key="key">
+        <!-- <div class="flex contact--title text-xl font-semibold w-48 h-7 left-44 top-60 text-center">KANTOR PUSAT</div>
+        <div class="flex contact--title text-xl font-semibold w-48 h-7 left-44 top-60 text-center">WORKSHOP</div>
+        <div class="flex contact--title text-xl font-semibold w-48 h-7 left-44 top-60 text-center">DIVISI TELKOM</div>
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-4"><img class="img--asset float-left mb-14 mr-4" :src="require('../assets/ornament/pin.png')">Jl. Raya Bayur No. 72 Periuk Jaya Tangerang Banten 15131</div>
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-4"><img class="img--asset float-left mb-14 mr-4" :src="require('../assets/ornament/pin.png')">Kp. Sekarwangi No. 39-40 Neglasari Tangerang Banten 15129</div>
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-4"><img class="img--asset float-left mb-14 mr-4" :src="require('../assets/ornament/pin.png')">Jl. Mandanau Blok A5 No 29 Tangerang Banten 15133</div>
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-3" v-for="(index, key) in dataEmail" :key="key">
                     <img :src="require(`../assets/ornament/mail.png`)" class="img--asset float-left mb-4 mr-4"/><a href="mailto:info@bayurjaya.co.id?subject=Mail from Our Site">info@bayurjaya.co.id</a>
           </div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-3"><img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')"><a href="tel:0215526610">+62 21 552 6610</a></div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-3"><img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')"><a href="tel:0215526610">+62 21 552 6610</a></div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-3"><img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')"><a href="tel:0215907462">+62 21 590 7462</a></div>
-        <div class="contact--title_isi text-base text-justify w-60 mt-3" v-for="(index, key) in dataClock" :key="key">
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-3"><img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')"><a href="tel:0215526610">+62 21 552 6610</a></div>
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-3"><img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')"><a href="tel:0215526610">+62 21 552 6610</a></div>
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-3"><img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')"><a href="tel:0215907462">+62 21 590 7462</a></div>
+        <div class="flex contact--title_isi text-base text-justify w-60 mt-3" v-for="(index, key) in dataClock" :key="key">
                     <img :src="require(`../assets/ornament/clock.png`)" class="img--asset float-left mb-4 mr-4"/>Senin – Jum’at. 8.00 – 17.00 Sabtu. 8.00 – 12.00
-          </div>
+          </div> -->
 
-        <!-- <div>
-          <p class="contact--title text-xl font-semibold w-48 h-7 left-44 top-60 text-center">KANTOR PUSAT</p>
+          <div>
+          <p class="contact--title text-xl font-semibold w-48 h-7 left-44 top-60 mb-6 text-center underline underline-offset-8 decoration-4 decoration-blue-900">KANTOR PUSAT</p>
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
             <img class="img--asset float-left mb-10 mr-4" :src="require('../assets/ornament/pin.png')">Jl. Raya Bayur No. 72 Periuk Jaya Tangerang Banten 15131</p>
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
@@ -29,10 +30,10 @@
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
             <img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')">+62 21 552 6610</p>
             <p class="contact--title_isi text-base text-justify w-60 mt-4">
-            <img class="img--asset float-left mb-4 mr-4 mt-3" :src="require('../assets/ornament/clock.png')">Senin – Jum’at. 8.00 – 17.00 Sabtu. 8.00 – 12.00</p>
+            <img class="img--asset float-left mb-10 mr-4 mt-3" :src="require('../assets/ornament/clock.png')">Senin – Jum’at. 8.00 – 17.00 Sabtu. 8.00 – 12.00</p>
         </div>
         <div>
-          <p class="contact--title col-span-2 text-xl font-semibold w-48 h-7 left-44 top-60">WORKSHOP</p>
+          <p class="contact--title col-span-2 text-xl font-semibold w-48 h-7 left-44 top-60 mb-6 underline underline-offset-8 decoration-4 decoration-blue-900">WORKSHOP</p>
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
             <img class="img--asset float-left mb-10 mr-4" :src="require('../assets/ornament/pin.png')">Kp. Sekarwangi No. 39-40 Neglasari Tangerang Banten 15129</p>
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
@@ -40,10 +41,10 @@
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
             <img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')">+62 21 552 6610</p>
             <p class="contact--title_isi text-base text-justify w-60 mt-4">
-            <img class="img--asset float-left mb-4 mr-4 mt-3" :src="require('../assets/ornament/clock.png')">Senin – Jum’at. 8.00 – 17.00 Sabtu. 8.00 – 12.00</p>
+            <img class="img--asset float-left mb-10 mr-4 mt-3" :src="require('../assets/ornament/clock.png')">Senin – Jum’at. 8.00 – 17.00 Sabtu. 8.00 – 12.00</p>
         </div>
         <div>
-          <p class="contact--title col-span-2 text-xl font-semibold w-48 h-7 left-44 top-60">DIVISI TELKOM</p>
+          <p class="contact--title col-span-2 text-xl font-semibold w-48 h-7 left-44 top-60 mb-6 underline underline-offset-8 decoration-4 decoration-blue-900">DIVISI TELKOM</p>
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
             <img class="img--asset float-left mb-10 mr-4" :src="require('../assets/ornament/pin.png')">Jl. Mandanau Blok A5 No 29 Tangerang Banten 15133</p>
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
@@ -51,9 +52,14 @@
           <p class="contact--title_isi text-base text-justify w-60 mt-4">
             <img class="img--asset float-left mb-4 mr-4" :src="require('../assets/ornament/telephone.png')">+62 21 590 7462</p>
             <p class="contact--title_isi text-base text-justify w-60 mt-4">
-            <img class="img--asset float-left mb-4 mr-4 mt-3" :src="require('../assets/ornament/clock.png')">Senin – Jum’at. 8.00 – 17.00 Sabtu. 8.00 – 12.00</p>
+            <img class="img--asset float-left mb-10 mr-4 mt-3" :src="require('../assets/ornament/clock.png')">Senin – Jum’at. 8.00 – 17.00 Sabtu. 8.00 – 12.00</p>
         </div>
-       -->
+      
+
+
+        </div>
+
+        
 
      </div>
      <div  class="contact--title text-3xl font-semibold not-italic mt-24">Deskripsikan Kebutuhan Anda, Kami Selalu Siap Membantu</div>
