9df076af72741ee76fe095f6169a3d62c55ba615
hanasyasabrina <83750473+hanasyasabrina@users.noreply.github.com>
2025-04-10 21:38:26 +0700

    test pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+// vue.config.js
+
+module.exports = {
+  publicPath: '/updateBayurJaya2023/', // ini wajib kalau pakai GitHub Pages
+};
+
+ 
